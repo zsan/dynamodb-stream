cf2287d98a99bbc6fa42dc8679ba3502ee3a0a88
zsan <zaksantanu@gmail.com>
2021-07-29 18:29:53 +0700

    stream setup

--- handler.py
+++ handler.py
@@ -1,7 +1,12 @@
 import json
 
-
 def hello(event, context):
+    event_attr = event["Records"]
+
+    print(event_attr)
+
+    # or you can do more processes here
+
     body = {
         "message": "Go Serverless v2.0! Your function executed successfully!",
         "input": event,
